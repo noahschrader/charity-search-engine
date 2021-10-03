249e268a29274b122dac3c80f2de9eb9cae1f012
Noah Schrader <noah.r.schrader@gmail.com>
2021-10-03 13:36:21 -0500

    fix module not found error when runnning app

--- charity/wsgi.py
+++ charity/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for charity-search-engine project.
+WSGI config for charity project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charity-search-engine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charity.settings')
 
 application = get_wsgi_application()
