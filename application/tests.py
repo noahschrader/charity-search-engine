06e8f5332bd88aa143413ea443deebe24f1cb84e
Noah Schrader <noah.r.schrader@gmail.com>
2021-10-07 16:22:59 -0500

    refactor get request to charity navigator api

--- application/tests.py
+++ application/tests.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
-import requests
+from application.api.charity_navigator import get_organizations
 
 
 class CharityNavigatorApi(TestCase):
     def testGetRequest(self):
-        query = {'app_id': '998e64be', 'app_key': 'c99ca3e66a3f61b839486371709a0cd4'}
-        response = requests.get('https://api.data.charitynavigator.org/v2/Organizations', query)
-        data = response.json()
-        self.assertTrue(data)
+        query = get_organizations({})
+        self.assertIsNotNone(query)
