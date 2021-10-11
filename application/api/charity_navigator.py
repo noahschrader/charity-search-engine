d78adc0d973b57d17d15ad0cd7f68a0399a8b5cd
Noah Schrader <noah.r.schrader@gmail.com>
2021-10-11 14:55:24 -0500

    basic search result template with query results

--- application/api/charity_navigator.py
+++ application/api/charity_navigator.py
@@ -4,15 +4,10 @@ app_id = '998e64be'
 app_key = 'c99ca3e66a3f61b839486371709a0cd4'
 
 base_url = 'https://api.data.charitynavigator.org/v2'
-base_query = {'app_id': app_id, 'app_key': app_key, 'pageSize': 1000, 'pageNum': 1, 'searchType': 'NAME_ONLY'}
+base_query = {'app_id': app_id, 'app_key': app_key, 'pageSize': 25, 'pageNum': 1, 'sort': 'RATING:DESC'}
 
 
 def get_organizations(query):
     query.update(base_query)
     response = requests.get(base_url + '/Organizations', query)
-    data = response.json()
-    while response.status_code == 200:
-        query['pageNum'] += 1
-        response = requests.get(base_url + '/Organizations', query)
-        data += response.json()
-    return data
+    return response.json()
--- application/views/search.py
+++ application/views/search.py
@@ -4,6 +4,9 @@ from application.api.charity_navigator import get_organizations
 
 
 class Search(View):
+    def get(self, request):
+        return render(request, "main/search.html")
+
     def post(self, request):
         charities = get_organizations({'search': request.POST['search']})
         return render(request, "main/search.html", {'search': request.POST['search'], 'charities': charities})
