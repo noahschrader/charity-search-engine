841135bc0c00aa28597573d428e565627d5bcc25
terryd300 <terry.r.delaney@gmail.com>
2021-10-12 15:39:42 -0500

    Started NameSearch View to submit the query and then parse into a dictionary for display

--- application/views.py
+++ application/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from api.charity_navigator import get_organizations
+import json
 
 # Create your views here.
+
+class NameSearch(View):
+    def get(self, request, query):
+        #  For the get method of NameSearch, receive the query, submit it to the API, and return the results
+        r = get_organizations({"search": query, "rated": "TRUE"})   # Send the query to the get_organizations method to obtain results from the API
+
+        return render(request, "main/home.html")
