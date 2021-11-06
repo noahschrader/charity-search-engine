1e12e8aadf17272e6a7e3bf6215ed1d4121ee0bf
Tanner <tanner.gabel.2016@gmail.com>
2021-11-05 22:41:28 -0500

    Improved home dynamics.
    - The home template now expects a list of buttons which have text and can have a selected state. This allows support for a variable number of buttons for the home button group.
    
    - Implemented a simple button class which can be used to pass to front end for variable button display.

--- application/views/home.py
+++ application/views/home.py
@@ -1,9 +1,9 @@
 import datetime
 import typing
+from typing import List, Dict
 
 from django.shortcuts import render
 from django.views import View
-from typing import List, Dict, Any
 
 from application.fema.FEMA import DisasterQuery, DateFilter, Filter, ApiHandler
 
@@ -24,11 +24,16 @@ def count_strings_in_list(lst: List[str]) -> Dict:
     return counts
 
 
-def lookup_recent_disasters() -> List[str]:
+def lookup_recent_disasters(scope_in_days=90) -> List[str]:
+    """
+    Queries the FEMA API for disasters that were declared a timeframe before today.
+    :param scope_in_days: The number of days in the past to consider for disasters.
+    :return: A list of disaster titles sorted from most declared to least declared.
+    """
     query = DisasterQuery()
 
     # Setup date filter for records from the last 30 days.
-    thirty_day_time_delta = datetime.timedelta(days=90)
+    thirty_day_time_delta = datetime.timedelta(days=scope_in_days)
     start_date = datetime.datetime.today() - thirty_day_time_delta
     date_filter = DateFilter(Filter.LogicalOperator.GREATER_THAN, start_date)
 
@@ -44,8 +49,40 @@ def lookup_recent_disasters() -> List[str]:
     return list(sorted_counts.keys())
 
 
-class Home(View):
-    def get(self, request):
-        top_disasters = lookup_recent_disasters()[0:10]
+class Button:
 
-        return render(request, "main/home.html", {"disasters": top_disasters})
+    def __init__(self, text="", is_selected=False):
+        """
+        :param text: The text to be displayed on the button.
+        :param is_selected: Indicates if a button is currently selected.
+        """
+        self.text = text
+        self.is_selected = is_selected
+
+    def get_text(self) -> str:
+        """
+        :return: The current text value of this button.
+        """
+        return self.text
+
+    def set_text(self, new_text) -> str:
+        """
+        Sets the text of the button.
+        :param new_text: Text to be set to.
+        :return: The old text.
+        """
+        prev = self.text
+        self.text = new_text
+        return prev
+
+
+class Home(View):
+
+    def get(self, request):
+        menu_buttons = [
+            vars(Button("Recent Disasters", True)),
+            vars(Button("Nearby Charities", False))
+        ]
+        print(menu_buttons)
+        top_disasters = lookup_recent_disasters()[0:10]
+        return render(request, "main/home.html", {"items": top_disasters, "menu_options": menu_buttons})
